aab9c80b3bd209ef03c3676f361b22f9c228b3c7
Gummy-168 <titikab55@gmail.com>
2024-10-28 15:49:53 +0700

    add all files

---
+++ all.js
@@ -0,0 +1,26 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
